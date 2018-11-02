0d200f2933fcf1b663ed1b27d1db15b2ae9f8481
zqyz1 <42383506+zqyz1@users.noreply.github.com>
2018-11-02 08:45:30 -0700

    Addrd to mad Libs

--- notes/Catherine Lozano - Mad Lib.py
+++ notes/Catherine Lozano - Mad Lib.py
@@ -12,7 +12,9 @@ bodypart_1 = input("WHat is a body part? ")
 bodypart_2 = input("What is another body part? ")
 
 print("The monster under my bed is %s" % adjective_1)
+print("The monster made me feel %s" % adjective_2)
+print("The monster was very %s" % adjective_3)
+print("The mosnter had a very tall %s" % noun_1)
+print("The monster first went into the big scary %s" % noun_2)
+print("I got very scared and ran into a %s" % noun_3)
 print("")
-print("")
-
-
